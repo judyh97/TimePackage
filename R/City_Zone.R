40e140910ec08617b305010c3e5f14d0224fed44
gatescao <gatescao@hotmail.com>
2017-07-08 00:08:51 +0200

    Added City_Zone.R

---
+++ R/City_Zone.R
@@ -0,0 +1,14 @@
+#' City timezones. 
+#'
+#' A dataset containing the world's major cities and their corresponding timezones. The variables are as follow:
+#' 
+#' \itemize{
+#'   \item City.
+#'   \item Zone.
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name City_Zone
+#' @usage data(City_Zone)
+#' @format A data frame with 428 rows and 2 variables
+NULL
